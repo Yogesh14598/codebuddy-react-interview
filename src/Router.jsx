69b2631d559ef93ee0521af8f6834cdc1943b2ca
Yogesh14598 <91667742+Yogesh14598@users.noreply.github.com>
2022-01-27 11:04:14 +0530

    Add files via upload

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Fallback from './Fallback';
+import Form1 from './pages/Form1';
+import Form2 from './pages/Form2';
+import Form3 from './pages/Form3';
 
 const Home = React.lazy(() => import('./pages/Home'));
 const Posts = React.lazy(() => import('./pages/Posts'));
@@ -11,6 +14,9 @@ const Router = () => (
       <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/posts" component={Posts} />
+        <Route exact path="/Form1" component={Form1} />
+        <Route exact path="/Form2" component={Form2} />
+        <Route exact path="/Form3" component={Form3} />
       </Switch>
     </BrowserRouter>
   </React.Suspense>
